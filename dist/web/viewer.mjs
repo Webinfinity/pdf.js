21cafaa413b9461f80b747aab8a614f6c9d1dc61
AlexanderRatomski <alexander.ratomski@ventionteams.com>
2025-10-31 11:35:54 +0100

    Revert "EM360-5479 test to trigger CodeQL"

--- dist/web/viewer.mjs
+++ dist/web/viewer.mjs
@@ -5497,7 +5497,7 @@ class DownloadManager {
       let viewerUrl;
       viewerUrl = "?file=" + encodeURIComponent(blobUrl + "#" + filename);
       if (dest) {
-        viewerUrl += `#${encodeURIComponent(dest)}`;//test to trigger CodeQL
+        viewerUrl += `#${encodeURIComponent(dest)}`;
       }
       try {
         window.open(viewerUrl);
